5e400e84893be18a6da0947e482fb6223adf2023
LeandroSQ <leandrosoaresquevedo@gmail.com>
2021-01-24 03:19:44 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# RedragonRGBController
-Reverse engineering of the Redragon's peripherals LED USB command
+# Redragon RGB Controller
+A NodeJS implementation for sending commands to Redragon peripherals using USB HID protocol.
+
+**Attention:** *This was made on top of reverse engineering of the peripherals USB protocols!*
+
+This project uses the wonderful library (node-hid)[https://github.com/node-hid/node-hid]
+
+# Usage
+- Install the dependencies `npm install`
+- (WINDOWS) Start a terminal instance on admin privileges
+- Change the constant 'COLOR' to your preferred color on the files 'keyboard.js' and 'mouse.js'
+- Run the specified target
+    - Run `node keyboard.js` if you want to control your keyboard
+    - Run `node mouse.js` if you want to control your mouse
+- You're done!
+
+# Available devices
+Currently tested with:
+- REDRAGON Griffin mouse *(Both lunar white and regular)*
+- REDRAGON Kala keyboard *(Lunar white, but should work the same with the black regular version)*
+
+*Both of them, my own devices that I used for revering the communication.*
+
+# Adding new devices
+It should be trivial to add other devices being the overall and over simplified process:
+- Starting USB capture WireShark with USBPCap
+- Changing the color (Or other property) on the original software
+- Stop the caputre
+- Analyse a pattern of "HOST -> DEVICE" then "DEVICE -> HOST" more than once and sequential
+- Usually it starts with the profile being set, the color, and the last one is the 'apply' command
+- After that is just a matter of changing the raw bytes on the code
+
+# TODO
+[x] - Reverse engineer the Redragon mouse single color command
+[x] - Reverse engineer the Redragon keybaord single color command
+[ ] - Reverse enginner the brightness value command
+[ ] - Add a command line argument parser, for passing the color as an argument
---
+++ mouse.js
@@ -0,0 +1,68 @@
+// IMPORTANT! This needs to be run as Administrator
+
+// Imports
+const HID = require("node-hid");
+
+// Constants
+const VENDOR_ID = 0x04D9;
+const PRODUCT_ID = 0xFC38;
+const RELEASE = 0x110;
+const INTERFACE = 0x2;
+const USAGE_PAGE = 0xFFA0;
+const USAGE = 0x1;
+const COLOR = "#FF0000";
+
+// Configuration
+HID.setDriverType("libusb");
+
+// Utility methods
+function hexToRgb(hex) {
+    let [red, green, blue] = hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b).substring(1).match(/.{2}/g).map(x => parseInt(x, 16));
+    return { red, green, blue };
+}
+
+function enumerateDevices() {
+    return HID.devices()
+        .filter(x => x.vendorId === VENDOR_ID)
+        .filter(x => x.productId === PRODUCT_ID)
+        .filter(x => x.release === RELEASE)
+        .filter(x => x.interface === INTERFACE)
+        .filter(x => x.usagePage === USAGE_PAGE)
+        .filter(x => x.usage === USAGE)
+        .filter(x => /mouse/gi.test(x.product));
+}
+
+function sendChangeColorCommand(device, { red, green, blue }) {
+    device.sendFeatureReport([0x02, 0xf3, 0x49, 0x04, 0x06, 0x00, 0x00, 0x00, red, green, blue, 0x01, 0x00, 0x2, 0x00, 0x00]);
+}
+
+function sendApplyChangesCommand(device) {
+    device.sendFeatureReport([0x02, 0xf1, 0x02, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+}
+
+function openDevice(deviceInfo, color) {
+    try {
+        let device = new HID.HID(deviceInfo.path);
+
+        sendChangeColorCommand(device, color);
+        sendApplyChangesCommand(device);
+
+        device.close();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Execution
+function main() {
+    // Convert the hex color to rgb
+    const color = hexToRgb(COLOR);
+
+    // List all hid devices that match the specified filter
+    let devices = enumerateDevices();
+
+    // For each device, open it and send the commands
+    devices.forEach(x => openDevice(x, color));
+}
+
+main();
